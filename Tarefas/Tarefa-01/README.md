1c2b74f72fe1775b654c1edfff056fb44699235a
Anny Caroline <annycarolinegnr@gmail.com>
2018-09-29 17:04:13 -0300

    Tarefa-01 - debounce

---
+++ Tarefas/Tarefa-01/README.md
@@ -0,0 +1,16 @@
+# Tarefa 01
+
+- Piscar o LED a cada 1 segundo
+- Botão 1: Acelerar o pisca-pisca a cada pressionamento
+	- (somente ao pressionar e não ao soltar)
+- Botão 2: Desacelerar a cada pressionamento
+	- (somente ao pressionar e não ao soltar)
+- Botão 1+2 (em menos de 500ms): Parar
+
+## Circuito
+
+| Pino | |
+----------
+| 2    | botão de aceleração (pull-down) |
+| 4    | botão de desaceleração (pull-down) |
+| 13   | led |
---
+++ Tarefas/Tarefa-01/Tarefa-01.ino
@@ -0,0 +1,150 @@
+/* clicks */
+unsigned long clickFirst;
+unsigned long clickSecond;
+int clickState = 0;
+/*
+ * 0: no button was clicked yet
+ * 1: the deceleration button was the first to be clicked
+ * 2: the acceleration button was the first to be clicked
+ * 3: the two buttons were clicked
+ */
+
+/* led */
+const int ledPin = 13;
+int ledState = LOW;
+int ledInterval = 1000;
+int ledIntervalMax = 2600;
+int ledIntervalDt = 200;
+unsigned long ledPreviusMillis;
+unsigned long ledCurrentMillis;
+
+/* deceleration button */
+const int desPin = 2;
+int desReading;
+int desLastState = LOW;
+int desState;
+int desLock = 1;
+unsigned long desLastDebounceTime = 0;
+
+/* acceleration button */
+const int accPin = 4;
+int accReading;
+int accLastState = LOW;
+int accState;
+int accLock = 1;
+unsigned long accLastDebounceTime = 0;
+
+/* buttons debounce */
+unsigned long debounceDelay = 25;
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(desPin, INPUT);
+  pinMode(accPin, INPUT);
+
+  ledPreviusMillis = millis();
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // deceleration button
+  desReading = digitalRead(desPin);
+
+  if (desReading != desLastState)
+    desLastDebounceTime = millis();
+
+  if ((millis() - desLastDebounceTime) > debounceDelay) {    
+    if (desReading != desState) {
+      desState = desReading;
+
+      if (!desState && desLock == 0) 
+          desLock = 1;
+    
+      if (desState && desLock == 1){
+          // increase the ledInterval
+          ledInterval += ledInterval <= ledIntervalMax - ledIntervalDt ? ledIntervalDt : 0;
+      
+          // update clickState
+          if (clickState == 0 || clickState == 1){
+            // clickState == 0: it is the first time that a button was clicked
+            // clickState == 1: the user is clicking deceleration button for the second time => reset clickFirst and maintain the clickState
+            clickFirst = millis();
+            clickState = 1;
+            
+          } else if (clickState == 2){
+            // the acelerate button was already clicked
+            clickSecond = millis();
+            clickState = 3;
+          }
+    
+          desLock = 0;   
+      }
+    }
+  } 
+  desLastState = desReading;
+
+  
+  // acceleration button 
+  accReading = digitalRead(accPin);
+
+  if (accReading != accLastState)
+    accLastDebounceTime = millis();
+
+  if ((millis() - accLastDebounceTime) > debounceDelay) {    
+    if (accReading != accState) {
+      accState = accReading;
+
+      if (!accState && accLock == 0)
+          accLock = 1;
+    
+      if (accState && accLock == 1){
+        // decrease the ledInterval
+        ledInterval -= (ledInterval >= ledIntervalDt) ? ledIntervalDt : 0;
+    
+        // update clickState
+        if (clickState == 0 || clickState == 2){
+          // clickState == 0: it is the first time that a button was clicked
+          // clickState == 2: the user is clicking acceleration button for the second time => reset clickFirst and maintain the clickState     
+          clickFirst = millis();
+          clickState = 2;
+          
+        } else if (clickState == 1){
+          // the deceleration button was already clicked
+          clickSecond = millis();
+          clickState = 3;
+        } 
+    
+        accLock = 0;
+      }
+    }
+  } 
+  accLastState = accReading;   
+   
+
+  // check the time between the clicks of the two buttons
+  if (clickState == 3){
+    if (clickSecond - clickFirst <= 500){
+      // the time between the clicks of the two buttons was less, or equal, to 500ms => the program gets stuck in the while loop
+      while(1);
+    }else{
+      // the time between the clicks of the two buttons was greater than 500ms, so the clicks are disregarded. In other words, it is considered that no button was clicked yet
+      clickState = 0;
+    }
+  }
+
+  // blink led
+  ledCurrentMillis = millis();
+  if (ledCurrentMillis - ledPreviusMillis > ledInterval){
+    ledState = !ledState;
+    
+    digitalWrite(LED_BUILTIN, ledState);
+    ledPreviusMillis = ledCurrentMillis;
+  }
+  
+  
+  
+}
+
+
